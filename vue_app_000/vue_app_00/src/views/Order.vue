ca6be7e468c0b3248037d3c315a9ff435d7d3deb
z <1570623976@qq.com>
2019-09-24 00:02:15 +0800

    放大镜

--- vue_app_000/vue_app_00/src/views/Order.vue
+++ vue_app_000/vue_app_00/src/views/Order.vue
@@ -12,8 +12,9 @@
                     </ul>
                 </section>
                 <section class="summary clearfix">
-                    <div class="summary-image fl clearfix">
-                        <div class="picture fl">
+                    <div class="summary-image  clearfix">
+                        
+                        <div class="picture ">
                             <ul>
                                 <li>
                                     <img :src="i==1 ? 'http://127.0.0.1:8080/image/order/9e238e44beb44f338a3ab6ceec185fc4X640.jpg':
@@ -24,19 +25,18 @@
                                 </li>
                             </ul>   
                         </div>
-                        <div class="thumbanil fl">
+                        <div id="mask" v-show="show" :style="maskStyle"></div>
+                        <div @mouseover="toggle" @mouseout="toggle" @mousemove="drag" id="super-mask"></div>
+                        <div id="div-lg" v-for="(item,i) of pics[0]" :key="i" v-show="show" :style="{backgroundUrl}"></div>   
+                    </div>
+                    <div class="thumbanil fl">
                             <ul class="clearfix" id="sm">
                                 <li v-for="(item,i) of pics[0]" :key="i" @click="changeone" >
                                     <img :src=" `http://127.0.0.1:8080/`+ item.displayimg" alt=""
                                         :data-id="item.order_watch_id">
                                 </li>
                             </ul>
-                        </div>   
-                    </div>
-                    <div id="mask" style="top:112px;left: 0;display: block;"></div>
-                    <div id="super-mask"></div>
-                    <div id="div-lg" style="top:112px;left: 570px;display: block;background: url(pics[0].displayimg) no-repeat;background-position:50px 50px; ">
-                    </div>
+                        </div>
                 </section>
             </main>
         </div>
@@ -48,8 +48,16 @@ export default {
     data(){
         return{
             breadcrumbs:["首页","表","浪琴","浪琴 Longines HERIAGE 经典复刻系列"],      //保存面包屑导航
-            pics:[],                 //保存服务器返回的图片
-            i:1
+            pics:[
+                {order_watch_id:1,displayimg:""}
+            ],                 //保存服务器返回的图片
+            i:1,
+            show:false,
+            maskStyle:{left:0, top:-112},
+            backgroundUrl:{
+                background:url('http://127.0.0.1:8080/image/order/9e238e44beb44f338a3ab6ceec185fc4X640.jpg')
+                
+            }
         }
     },
     created() {
@@ -57,14 +65,14 @@ export default {
     },
     methods:{
         loadMore(){
-        // 1.创建url地址
-        var url = "order";
-        // 2.发送ajax请求
-        this.axios.get(url).then(res=>{
-            //console.log(res);
-            this.pics = res.data.data; 
-            //console.log(this.pics)
-            })
+            // 1.创建url地址
+            var url = "order";
+            // 2.发送ajax请求
+            this.axios.get(url).then(res=>{
+                //console.log(res);
+                this.pics = res.data.data; 
+                //console.log(this.pics)
+                })
         },
         // 完成展示表的切换
         changeone(e){
@@ -72,8 +80,36 @@ export default {
                 var id = e.target.dataset.id;
                 this.i = id;
             }
-        }
-    },
+        },
+        //当鼠标在super-mask div上移动时
+        drag(e){
+            //通过事件对象获得鼠标相对于div左上角的偏移量。-mask的一半大小，就算出mask左上角相对于div左上角的left和top值
+            var left=e.offsetX-80;
+            var top=e.offsetY-80;
+            //如果mask左边越界，就停留在最左边，而不超出
+            if(left<0){left=0}
+            //否则如果left超过mImg的宽-mask的宽，说明右边越界，就停留在最右边，而不超出。
+            else if(left>240){left=240}
+            //如果mask上边越界，就停留在最上边，而不超出
+            if(top<0){top=0}
+            //否则如果top超过mImg的高-mask的高，说明下边越界，就停留在最下边，而不超出。
+            else if(top>240){top=240}
+            //为最终计算结果left和top添加单位
+            left=left+"px";
+            top=top+"px";
+            //修改maskStyle对象，自动触发页面mask元素位置的更改。
+            this.maskStyle={left,top};
+        },
+        toggle(){
+            this.show=!this.show;
+        },
+        // computed:{
+        //     gbPosition(){
+        //     return `${-parseInt(this.maskStyle.left)*20/9}px ${-parseInt(this.maskStyle.top)*20/9}px`;
+        //     }
+        // },
+        
+    }
 }
 </script>
 <style scoped>
